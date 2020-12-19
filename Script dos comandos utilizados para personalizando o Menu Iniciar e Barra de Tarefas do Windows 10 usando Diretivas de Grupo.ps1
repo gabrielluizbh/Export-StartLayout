4745bfce2fa38f60b0f08d49980f9fb70311270f
Gabriel Luiz <gabrielluiz@msn.com>
2020-12-19 14:30:43 -0300

    Add files via upload

---
+++ Script dos comandos utilizados para personalizando o Menu Iniciar e Barra de Tarefas do Windows 10 usando Diretivas de Grupo.ps1
@@ -0,0 +1,28 @@
+﻿# Script dos comandos utilizados para personalizar o Menu Iniciar e Barra de Tarefas do Windows 10 usando Diretivas de Grupo - Créditos Gabriel Luiz - www.gabrielluiz.com ##
+
+
+# Comandos cria o diretório XML. O segundo comando cria um compartilhamento de arquivo chamado XML
+
+New-Item -ItemType Directory -Path C:\XML
+New-SmbShare -Path C:\XML\ -Name XML
+
+# Comandos de exportação de layout do Menu Iniciar.
+
+Export-StartLayout –path C:\XML\layout-menu-iniciar.xml
+
+<# 
+Referências:
+
+https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/customize-start-layout?WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/pt-br/windows/configuration/configure-windows-10-taskbar?WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/pt-br/windows/configuration/customize-and-export-start-layout?WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/pt-br/windows/configuration/customize-windows-10-start-screens-by-using-group-policy?WT.mc_id=WDIT-MVP-5003815
+
+https://sccmguru.wordpress.com/2016/08/18/windows-10-1607-taskbar-and-start-customization-deep-dive/
+
+https://docs.microsoft.com/pt-br/archive/blogs/deploymentguys/windows-10-start-layout-customization?WT.mc_id=WDIT-MVP-5003815
+
+#>
